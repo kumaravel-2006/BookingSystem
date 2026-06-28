461686d8aaac7805aaf09c5fbd5843ecd5d386dc
kumaravel <svkumaravel2006@gmail.com>
2026-06-28 20:38:09 +0530

    queue_service fixed

--- frontend/client/src/services/queueService.js
+++ frontend/client/src/services/queueService.js
@@ -1,17 +1,28 @@
-import api from "./api";
+import axios from 'axios'
+import { getToken } from '../utils/jwtUtils'
+
+const queueApi = axios.create({
+  baseURL: 'http://localhost:8083'
+})
+
+queueApi.interceptors.request.use((config) => {
+  const token = getToken()
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
+})
 
 const joinQueue = async (eventId) => {
-    const res = await api.post('/queue/join', { eventId })
+    const res = await queueApi.post('/queue/join', { eventId })
     return res.data
 }
 
 const leaveQueue = async (eventId) => {
-    const res = await api.post('/queue/leave', { eventId })
+    const res = await queueApi.post('/queue/leave', { eventId })
     return res.data
 }
 
 const getQueueStatus = async (eventId) => {
-    const res = await api.get('/queue/status', { params: { eventId } })
+    const res = await queueApi.get('/queue/status', { params: { eventId } })
     return res.data
 }
 
